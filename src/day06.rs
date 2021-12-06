99a17f9f8936de31a63644b7baa8d6f00b4b5293
Strackeror <rremilavergne@gmail.com>
2021-12-07 00:21:23 +0100

    day6 functional

--- src/day06.rs
+++ src/day06.rs
@@ -1,26 +1,20 @@
-use itertools::Itertools;
-
+use std::vec;
 pub fn day06(path: &str) -> anyhow::Result<()> {
     let input: Vec<usize> = std::fs::read_to_string(path)?
         .split(',')
         .map(|l| l.parse().unwrap())
         .collect();
-    let states = (0..=8)
-        .map(|i| {
-            (0..128).fold(vec![i], |acc, _| {
-                acc.iter()
-                    .flat_map(|f| match f {
-                        0 => vec![6, 8],
-                        n => vec![n - 1],
-                    })
-                    .collect()
-            })
-        })
-        .collect_vec();
+
+    let step = |v: Vec<_>, _| vec![v[1], v[2], v[3], v[4], v[5], v[6], v[7] + v[0], v[8], v[0]];
     let count: usize = input
         .iter()
-        .map(|i| states[*i].iter().map(|i| states[*i].len()).sum::<usize>())
+        .map(|f| {
+            let mut init = vec![0usize; 9];
+            init[*f] = 1;
+            (0..256).fold(init, step).iter().sum::<usize>()
+        })
         .sum();
-    dbg!(count);
+    println!("count:{}", count);
+
     Ok(())
 }
