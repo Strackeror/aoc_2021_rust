a9afcd9bcc6c9681fb6a57ae5f7bea4131b422ce
Strackeror <rremilavergne+git@gmail.com>
2021-12-19 20:30:00 +0100

    day19 part1 working example ?

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,168 @@ mod day16;
 mod day17;
 mod day18;
 
+mod day19_old;
+
+mod day19 {
+    use std::{
+        collections::{BTreeMap, HashMap, HashSet, VecDeque},
+        ops::{Neg, Not, Sub},
+    };
+
+    use anyhow::Result;
+    use itertools::Itertools;
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+    struct Vector(i32, i32, i32);
+
+    impl Sub for Vector {
+        type Output = Vector;
+
+        fn sub(self, rhs: Self) -> Self::Output {
+            self + -rhs
+        }
+    }
+    impl Neg for Vector {
+        type Output = Vector;
+
+        fn neg(self) -> Self::Output {
+            Vector(-self.0, -self.1, -self.2)
+        }
+    }
+
+    impl std::ops::Add for Vector {
+        type Output = Vector;
+
+        fn add(self, rhs: Self) -> Self::Output {
+            Vector(self.0 + rhs.0, self.1 + rhs.1, self.2 + rhs.2)
+        }
+    }
+
+    fn all_rotations() -> Vec<((usize, usize, usize), (i32, i32, i32))> {
+        [0, 1, 2]
+            .into_iter()
+            .permutations(3)
+            .map(|v| v.into_iter().collect_tuple().unwrap())
+            .cartesian_product(
+                [-1, 1]
+                    .into_iter()
+                    .cartesian_product([-1, 1].into_iter())
+                    .cartesian_product([-1, 1].into_iter())
+                    .map(|((a, b), c)| (a, b, c)),
+            )
+            .collect()
+    }
+
+    fn apply_rotation(v: Vector, rot: ((usize, usize, usize), (i32, i32, i32))) -> Vector {
+        let v = [v.0, v.1, v.2];
+
+        Vector(
+            v[rot.0 .0] * rot.1 .0,
+            v[rot.0 .1] * rot.1 .1,
+            v[rot.0 .2] * rot.1 .2,
+        )
+    }
+
+    fn diff_list(points: &[Vector]) -> Vec<(usize, usize, Vector)> {
+        points
+            .iter()
+            .copied()
+            .enumerate()
+            .tuple_combinations::<(_, _)>()
+            .map(|((idx_a, point_a), (idx_b, point_b))| (idx_a, idx_b, point_b - point_a))
+            .flat_map(|(a, b, diff)| vec![(a, b, diff), (b, a, -diff)])
+            .collect_vec()
+    }
+
+    fn match_coord(a: &[Vector], b: &[Vector]) -> Vec<Vector> {
+        let a_diffs = diff_list(a);
+
+        let mut b_rotated = Vec::new();
+        let mut matching_diffs = Vec::new();
+        for rotation in all_rotations() {
+            b_rotated = b.iter().map(|v| apply_rotation(*v, rotation)).collect_vec();
+            let n_matching_diffs = diff_list(&b_rotated)
+                .into_iter()
+                .filter(|&(_, _, diff)| a_diffs.iter().any(|&(_, _, a_diff)| a_diff == diff))
+                .collect_vec();
+            if n_matching_diffs.len() >= 2 {
+                matching_diffs = n_matching_diffs;
+                break;
+            }
+        }
+
+        let map = matching_diffs
+            .iter()
+            .map(|&(b1, b2, diff)| {
+                (
+                    b1,
+                    a_diffs
+                        .iter()
+                        .filter(|&&(_, _, adiff)| adiff == diff)
+                        .collect_vec(),
+                )
+            })
+            .filter(|(_, found)| found.iter().all(|&&d| d.0 == found[0].0))
+            .map(|(a, b)| (a, b[0].0))
+            .collect::<HashMap<_, _>>();
+
+        dbg!(&map);
+        if map.len() < 12 {
+            return vec![];
+        }
+
+        let first = map.keys().next().unwrap();
+        let diff = b_rotated[*first] - a[map[first]];
+        b_rotated.into_iter().map(|v| v - diff).collect()
+    }
+
+    pub fn run(path: &str) -> Result<()> {
+        let content = std::fs::read_to_string(path)?;
+        let coords: Vec<Vec<Vector>> = content
+            .split("\n\n")
+            .map(|section| {
+                section
+                    .lines()
+                    .filter(|line| !line.starts_with("---"))
+                    .map(|coord_string| {
+                        coord_string
+                            .split(',')
+                            .map(|s| str::parse(s).unwrap())
+                            .collect_tuple()
+                            .unwrap()
+                    })
+                    .map(|(a, b, c)| Vector(a, b, c))
+                    .collect_vec()
+            })
+            .collect_vec();
+
+        let mut not_visited = (1..coords.len()).collect::<HashSet<_>>();
+        let mut queue = VecDeque::from([coords[0].clone()]);
+        let mut beacons = vec![coords[0].clone()];
+        while queue.is_empty().not() {
+            let next = queue.pop_front().unwrap();
+            dbg!("pop");
+
+            for &i in not_visited.clone().iter() {
+                dbg!(("check", i));
+                let matched = match_coord(&next, &coords[i]);
+                dbg!(matched.len());
+                if !matched.is_empty() {
+                    dbg!(("push", i));
+                    beacons.push(matched.clone());
+                    queue.push_back(matched);
+                    not_visited.remove(&i);
+                }
+            }
+        }
+
+        let matched_beacons = beacons.iter().flatten().copied().collect::<HashSet<_>>();
+        dbg!(&matched_beacons, matched_beacons.len());
+        dbg!(not_visited);
+
+        Ok(())
+    }
+}
 fn main() {
     let args = std::env::args().collect_vec();
     let input = args[1].clone();
@@ -43,9 +205,10 @@ fn main() {
             "day15" => day15::run(&input),
             "day16" => day16::run(&input),
             "day17" => day17::run(&input),
+            "day18" => day18::run(&input),
             _ => panic!("unexpected arg"),
         },
-        None => day18::run(&input),
+        None => day19::run(&input),
     }
     .unwrap();
 }
